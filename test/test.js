ee08dcf18c624551e4e4828c47c1cfb1332d6765
swe7103devops <93386138+swe7103devops@users.noreply.github.com>
2022-01-09 14:12:45 +0000

    Update test.js

--- test/test.js
+++ test/test.js
@@ -5,11 +5,5 @@ describe('Array', function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
-  describe('Array', function() {
-  describe('#indexOf()', function() {
-    // pending test below
-    it('should return -1 when the value is not present');
-  });
-});
 
 });
